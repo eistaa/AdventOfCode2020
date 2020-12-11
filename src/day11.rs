cc8385852932a962bdc1ef524e8b88cadaf361d3
Eivind Aarnæs <mail@eistaa.com>
2020-12-11 14:04:39 +0100

    Encapsulate game of life-like logic.

--- src/day11.rs
+++ src/day11.rs
@@ -13,6 +13,12 @@ enum Cell {
     Taken,
 }
 
+impl Default for Cell {
+    fn default() -> Self {
+        Cell::Floor
+    }
+}
+
 impl TryFrom<char> for Cell {
     type Error = String;
 
@@ -26,153 +32,241 @@ impl TryFrom<char> for Cell {
     }
 }
 
-fn parse(filename: &Path) -> Result<Vec<Vec<Cell>>, String> {
-    fs::read_to_string(filename)
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Default)]
+struct Point {
+    pub x: isize,
+    pub y: isize,
+}
+
+impl From<(isize, isize)> for Point {
+    fn from(p: (isize, isize)) -> Self {
+        Self { x: p.0, y: p.1 }
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+struct Grid {
+    cells: Vec<Cell>,
+    pub height: usize,
+    pub width: usize,
+}
+
+impl Grid {
+    pub fn new(height: usize, width: usize) -> Self {
+        let mut cells = Vec::with_capacity(height * width);
+        cells.resize_with(height * width, Default::default);
+
+        Grid { cells, height, width }
+    }
+
+    pub fn get(&self, x: usize, y: usize) -> Option<&Cell> {
+        if x >= self.width || y >= self.height {
+            None
+        } else {
+            self.cells.get(y * self.width + x)
+        }
+    }
+
+    pub fn get_mut(&mut self, x: usize, y: usize) -> Option<&mut Cell> {
+        if x >= self.width || y >= self.height {
+            None
+        } else {
+            self.cells.get_mut(y * self.width + x)
+        }
+    }
+
+    pub fn adjacent_next(&self, origin: &Point) -> [Option<&Cell>; 8] {
+        [
+            self.get_offset(origin, &(-1, -1).into()),
+            self.get_offset(origin, &(-1, 0).into()),
+            self.get_offset(origin, &(-1, 1).into()),
+            self.get_offset(origin, &(0, -1).into()),
+            self.get_offset(origin, &(0, 1).into()),
+            self.get_offset(origin, &(1, -1).into()),
+            self.get_offset(origin, &(1, 0).into()),
+            self.get_offset(origin, &(1, 1).into()),
+        ]
+    }
+
+    pub fn adjacent_nearest<F>(&self, origin: &Point, op: F) -> [Option<&Cell>; 8]
+    where
+        F: Fn(&Cell) -> bool,
+    {
+        [
+            self.scan_delta(origin, &(-1, -1).into(), &op),
+            self.scan_delta(origin, &(-1, 0).into(), &op),
+            self.scan_delta(origin, &(-1, 1).into(), &op),
+            self.scan_delta(origin, &(0, -1).into(), &op),
+            self.scan_delta(origin, &(0, 1).into(), &op),
+            self.scan_delta(origin, &(1, -1).into(), &op),
+            self.scan_delta(origin, &(1, 0).into(), &op),
+            self.scan_delta(origin, &(1, 1).into(), &op),
+        ]
+    }
+
+    pub fn get_offset(&self, origin: &Point, offset: &Point) -> Option<&Cell> {
+        let x = origin.x + offset.x;
+        let y = origin.y + offset.y;
+
+        if x >= 0 && y >= 0 {
+            self.get(x as usize, y as usize)
+        } else {
+            None
+        }
+    }
+
+    pub fn scan_delta<F>(&self, origin: &Point, delta: &Point, op: &F) -> Option<&Cell>
+    where
+        F: Fn(&Cell) -> bool,
+    {
+        let mut x = origin.x;
+        let mut y = origin.y;
+
+        if delta.x != 0 || delta.y != 0 {
+            loop {
+                if (delta.x == 0 || x + delta.x >= 0) && (delta.y == 0 || y + delta.y >= 0) {
+                    x = x + delta.x;
+                    y = y + delta.y;
+
+                    let cell = self.get(x as usize, y as usize)?;
+                    if op(cell) {
+                        break Some(cell);
+                    }
+                } else {
+                    break None;
+                }
+            }
+        } else {
+            None
+        }
+    }
+
+    pub fn simulate<FR, FT>(&mut self, rule: FR, terminate: FT)
+    where
+        FR: Fn(&Self, Point, &Cell) -> Cell,
+        FT: Fn(&Self, &Self) -> bool,
+    {
+        let mut next = self.clone();
+        loop {
+            for (i, cell) in self.cells.iter().enumerate() {
+                next.cells[i] = rule(
+                    &self,
+                    Point {
+                        x: (i % self.width) as isize,
+                        y: (i / self.width) as isize,
+                    },
+                    cell,
+                );
+            }
+
+            std::mem::swap(&mut self.cells, &mut next.cells);
+            if terminate(self, &next) {
+                break;
+            }
+        }
+    }
+
+    pub fn cell_count(&self, cell: &Cell) -> usize {
+        self.cells.iter().filter(|&c| c == cell).count()
+    }
+}
+
+fn parse(filename: &Path) -> Result<Grid, String> {
+    let data = fs::read_to_string(filename)
         .map_err(|err| format!("Failed to read data for day 11: {}", err))?
         .lines()
         .map(|line| line.chars().map(Cell::try_from).collect::<Result<Vec<Cell>, _>>())
-        .collect::<Result<Vec<Vec<Cell>>, _>>()
-}
+        .collect::<Result<Vec<Vec<Cell>>, _>>()?;
 
-fn adjacent_pt1(grid: &Vec<Vec<Cell>>, x: usize, y: usize) -> [Option<Cell>; 8] {
-    [
-        if y > 0 && x > 0 {
-            grid.get(y - 1).map(|row| row.get(x - 1)).flatten().copied()
-        } else {
-            None
-        },
-        if y > 0 {
-            grid.get(y - 1).map(|row| row.get(x)).flatten().copied()
-        } else {
-            None
-        },
-        if y > 0 {
-            grid.get(y - 1).map(|row| row.get(x + 1)).flatten().copied()
-        } else {
-            None
-        },
-        if x > 0 {
-            grid.get(y).map(|row| row.get(x - 1)).flatten().copied()
-        } else {
-            None
-        },
-        grid.get(y).map(|row| row.get(x + 1)).flatten().copied(),
-        if x > 0 {
-            grid.get(y + 1).map(|row| row.get(x - 1)).flatten().copied()
-        } else {
-            None
-        },
-        grid.get(y + 1).map(|row| row.get(x)).flatten().copied(),
-        grid.get(y + 1).map(|row| row.get(x + 1)).flatten().copied(),
-    ]
-}
-
-fn scan_grid(grid: &Vec<Vec<Cell>>, delta: (isize, isize), origin: (usize, usize)) -> Option<Cell> {
-    let mut x = origin.0 as isize;
-    let mut y = origin.1 as isize;
-
-    loop {
-        if (delta.0 == 0 || x + delta.0 >= 0) && (delta.1 == 0 || y + delta.1 >= 0) {
-            x = x + delta.0;
-            y = y + delta.1;
-            let cell = grid.get(y as usize)?.get(x as usize)?;
-            match cell {
-                Cell::Floor => continue,
-                Cell::Taken | Cell::Empty => break Some(*cell),
-            }
-        } else {
-            break None;
+    let mut grid = Grid::new(data.len(), data[0].len());
+    for (y, row) in data.iter().enumerate() {
+        if row.len() != grid.width {
+            Err(format!(
+                "Not all rows have the same length: expected {}, found {}",
+                grid.width,
+                row.len()
+            ))?
+        }
+        for (x, cell) in row.iter().enumerate() {
+            *grid.get_mut(x, y).unwrap() = *cell;
         }
     }
-}
 
-fn adjacent_pt2(grid: &Vec<Vec<Cell>>, x: usize, y: usize) -> [Option<Cell>; 8] {
-    [
-        scan_grid(grid, (-1, -1), (x, y)),
-        scan_grid(grid, (-1, 0), (x, y)),
-        scan_grid(grid, (-1, 1), (x, y)),
-        scan_grid(grid, (0, -1), (x, y)),
-        scan_grid(grid, (0, 1), (x, y)),
-        scan_grid(grid, (1, -1), (x, y)),
-        scan_grid(grid, (1, 0), (x, y)),
-        scan_grid(grid, (1, 1), (x, y)),
-    ]
-}
-
-fn cell_rule(cell: &Cell, adjacent: &[Option<Cell>; 8], taken_count: usize) -> Cell {
-    match cell {
-        Cell::Floor => *cell,
-        Cell::Empty => {
-            if !adjacent.contains(&Some(Cell::Taken)) {
-                Cell::Taken
-            } else {
-                *cell
-            }
-        }
-        Cell::Taken => {
-            if adjacent.iter().filter(|&&c| c == Some(Cell::Taken)).count() >= taken_count {
-                Cell::Empty
-            } else {
-                *cell
-            }
-        }
-    }
+    Ok(grid)
 }
 
 pub fn part01(filename: &Path) -> Result<String, String> {
     let mut grid = parse(filename)?;
-    let height = grid.len();
-    let width = grid[0].len();
 
-    loop {
-        let mut next = Vec::with_capacity(height);
-        for y in 0..height {
-            let mut row = Vec::with_capacity(width);
-            for x in 0..width {
-                row.push(cell_rule(&grid[y][x], &adjacent_pt1(&grid, x, y), 4));
+    grid.simulate(
+        |grid, at, cell| match cell {
+            Cell::Floor => Cell::Floor,
+            Cell::Empty => {
+                if grid.adjacent_next(&at).iter().any(|&cell| cell == Some(&Cell::Taken)) {
+                    Cell::Empty
+                } else {
+                    Cell::Taken
+                }
             }
-            next.push(row);
-        }
-        if next == grid {
-            break;
-        } else {
-            grid = next;
-        }
-    }
+            Cell::Taken => {
+                if grid
+                    .adjacent_next(&at)
+                    .iter()
+                    .filter(|&&cell| cell == Some(&Cell::Taken))
+                    .count()
+                    >= 4
+                {
+                    Cell::Empty
+                } else {
+                    Cell::Taken
+                }
+            }
+        },
+        |grid, prev| grid == prev,
+    );
 
     Ok(format!(
         "Seats taken when stabilized: {}",
-        grid.iter()
-            .map(|row| row.iter().filter(|&&cell| cell == Cell::Taken).count())
-            .sum::<usize>()
+        grid.cell_count(&Cell::Taken)
     ))
 }
 
 pub fn part02(filename: &Path) -> Result<String, String> {
     let mut grid = parse(filename)?;
-    let height = grid.len();
-    let width = grid[0].len();
 
-    loop {
-        let mut next = Vec::with_capacity(height);
-        for y in 0..height {
-            let mut row = Vec::with_capacity(width);
-            for x in 0..width {
-                row.push(cell_rule(&grid[y][x], &adjacent_pt2(&grid, x, y), 5));
+    grid.simulate(
+        |grid, at, cell| match cell {
+            Cell::Floor => Cell::Floor,
+            Cell::Empty => {
+                if grid
+                    .adjacent_nearest(&at, |&cell| cell != Cell::Floor)
+                    .iter()
+                    .any(|&cell| cell == Some(&Cell::Taken))
+                {
+                    Cell::Empty
+                } else {
+                    Cell::Taken
+                }
             }
-            next.push(row);
-        }
-        if next == grid {
-            break;
-        } else {
-            grid = next;
-        }
-    }
+            Cell::Taken => {
+                if grid
+                    .adjacent_nearest(&at, |&cell| cell != Cell::Floor)
+                    .iter()
+                    .filter(|&&cell| cell == Some(&Cell::Taken))
+                    .count()
+                    >= 5
+                {
+                    Cell::Empty
+                } else {
+                    Cell::Taken
+                }
+            }
+        },
+        |grid, prev| grid == prev,
+    );
 
     Ok(format!(
         "Seats taken when stabilized: {}",
-        grid.iter()
-            .map(|row| row.iter().filter(|&&cell| cell == Cell::Taken).count())
-            .sum::<usize>()
+        grid.cell_count(&Cell::Taken)
     ))
 }
